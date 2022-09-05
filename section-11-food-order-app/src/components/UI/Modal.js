e3790f69f58c271d94877f0dc109b24c8db0a892
Lucas Jandrew <fapiko@gmail.com>
2022-09-05 16:00:49 -0400

    Finished section 11

---
+++ section-11-food-order-app/src/components/UI/Modal.js
@@ -0,0 +1,25 @@
+import classes from './Modal.module.css';
+import {createPortal} from 'react-dom';
+
+const Backdrop = props => {
+    return <div className={classes.backdrop} onClick={props.onClose}/>
+}
+
+const ModalOverlay = props => {
+    return <div className={classes.modal}>
+        <div className={classes.content}>{props.children}</div>
+    </div>
+}
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+    return (
+        <>
+            {createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
+            {createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+        </>
+    );
+}
+
+export default Modal;
